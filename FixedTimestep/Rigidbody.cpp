e773417b31b275ecec52fc5f485328d295659a78
Scott AIE <sloth.13@hotmail.com>
2018-02-13 10:42:53 +1030

    Up to rotation added friction and elastic things

--- FixedTimestep/Rigidbody.cpp
+++ FixedTimestep/Rigidbody.cpp
@@ -2,7 +2,7 @@
 
 
 
-Rigidbody::Rigidbody(ShapeType shapeID, glm::vec2 position, glm::vec2 velocity, float rotation, float mass):PhysicsObject(shapeID), m_position(position), m_velocity(velocity), m_rotation(rotation), m_mass(mass)
+Rigidbody::Rigidbody(ShapeType shapeID, glm::vec2 position, glm::vec2 velocity, float rotation, float mass) :PhysicsObject(shapeID), m_position(position), m_velocity(velocity), m_rotation(rotation), m_mass(mass), m_angularVelocity(0), m_linearDrag(0.3f), m_angularDrag(0.3f), m_elasticity(1), m_moment(0)
 {
 }
 
@@ -13,6 +13,14 @@ Rigidbody::~Rigidbody()
 void Rigidbody::fixedUpdate(glm::vec2 gravity, float timeStep)
 {
 	if (!m_static) {
+		if (glm::length(m_velocity) < MIN_LINEAR_THRESHOLD) {
+			m_velocity = glm::vec2(0, 0);
+		}
+		if (glm::abs(m_angularVelocity) < MIN_ROTATION_THRESHOL) {
+			m_angularVelocity = 0;
+		}
+		m_velocity -= m_velocity * m_linearDrag * timeStep;
+		m_angularVelocity -= m_angularVelocity * m_angularDrag * timeStep;
 		m_position += m_velocity * timeStep;
 		applyForce(gravity * m_mass * timeStep);
 	}
@@ -48,8 +56,8 @@ void Rigidbody::resolveCollisions(Rigidbody * actor2)
 	glm::vec2 normal = glm::normalize(actor2->getPosition() - m_position);
 	glm::vec2 relativeVelocity = actor2->getVelocity() - m_velocity;
 	if (glm::dot(relativeVelocity, normal) > 0) return; //Stops being stuck
-	float elasticity = 1; 
-	float j = glm::dot(-(1.0f + elasticity) * (relativeVelocity), normal) / glm::dot(normal, normal * ((1.0f / m_mass) + (1.0f / actor2->getMass()))); 
+	float elasticity = (m_elasticity + actor2->getElasticity())/2.0f; 
+	float j = glm::dot(-(1.0f + elasticity) * (relativeVelocity), normal) / glm::dot(normal, normal * ((1.0f / m_mass) + (1.0f / actor2->getMass())));
 	if (m_static) {
 		//j = glm::dot(-(1 + elasticity) * (relativeVelocity), normal) / glm::dot(normal, normal * (1 / actor2->getMass())); // this is static
 	}
--- FixedTimestep/Rigidbody.h
+++ FixedTimestep/Rigidbody.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "PhysicsObject.h"
+#define MIN_LINEAR_THRESHOLD 0.01f
+#define MIN_ROTATION_THRESHOL 0.01f
+
 class Rigidbody : public PhysicsObject
 {
 public:
@@ -19,13 +22,20 @@ public:
 	float getRotation() { return m_rotation; }
 	glm::vec2 getVelocity() { return m_velocity; }
 	float getMass() { return m_mass; }
-
+	float getLinearDrag() { return m_linearDrag; }
+	float getAngularDrag() { return m_angularDrag; }
+	float getElasticity() { return m_elasticity; }
 
 protected:
 	glm::vec2 m_position;
 	glm::vec2 m_velocity;
+	float m_angularVelocity;
+	float m_moment;
 	float m_mass;
-	float m_rotation;	//2D so we only need a single float to represent our rotation
+	float m_rotation;	//2D so we only need a single float to represent our rotation //MAKE RADIANS
+	float m_linearDrag;
+	float m_angularDrag;
+	float m_elasticity;
 	bool m_static = false;
 };
 
